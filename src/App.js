ba0faf2e6b7598d8522f7dd1d412ee2b7b03cf5a
ViniciusRCampos <vinicius_uo@hotmail.com>
2022-09-08 16:23:20 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Products from './components/Products';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <BrowserRouter>
+        <p>Frontend-Online-Store</p>
+        <Switch>
+          <Route exact path="/" component={ Products } />
+        </Switch>
+      </BrowserRouter>
+    </section>
   );
 }
 
