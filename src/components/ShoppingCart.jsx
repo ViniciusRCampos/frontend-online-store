897e754df0ac395b8fbe678967a9f73026d19ef8
igopereira1 <igopereira1@gmail.com>
2022-09-08 17:20:28 -0300

    Requisito 3

---
+++ src/components/ShoppingCart.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ShoppingCart extends Component {
+  render() {
+    return (
+      <p data-testid="shopping-cart-empty-message">
+        Seu carrinho está vazio
+      </p>
+    );
+  }
+}
