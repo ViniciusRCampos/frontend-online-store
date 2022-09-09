6e6dfc0de5d92c28aa4139bee5f6d7b8ffe8a074
ojimaluis <106823349+ojimaluis@users.noreply.github.com>
2022-09-09 22:26:03 +0200

    local storage na api.js

--- src/services/api.js
+++ src/services/api.js
@@ -16,3 +16,12 @@ export async function getProductById() {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
 }
+
+// usando referencia do stackoverflow https://stackoverflow.com/questions/2010892/how-to-store-objects-in-html5-localstorage
+export function setLocalProducts(obj, products) {
+  localStorage.setItem(obj, JSON.stringify(products));
+}
+// acessar os produtos do local storage:
+export function getLocalProducts(obj) {
+  return JSON.parse(localStorage.getItem(obj));
+}
