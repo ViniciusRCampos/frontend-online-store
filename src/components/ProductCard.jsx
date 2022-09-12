11082d7c11cbcf1d9560bbec158697c926787505
ojimaluis <106823349+ojimaluis@users.noreply.github.com>
2022-09-12 19:37:45 +0200

    subindo parte do requisito 8

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,14 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
+import { setLocalProducts, getLocalProducts } from '../services/api';
 
 export default class ProductCard extends Component {
+  saveToLocalStorage = (obj) => {
+    const savedProduct = getLocalProducts('produtos') || [];
+    setLocalProducts('produtos', [...savedProduct, obj]);
+  };
+
   render() {
     const { title, thumbnail,
       price, id } = this.props;
@@ -10,9 +17,21 @@ export default class ProductCard extends Component {
         key={ id }
         data-testid="product"
       >
-        <img src={ thumbnail } alt={ title } />
-        <h2>{title}</h2>
-        <h3>{price}</h3>
+        <Link
+          to={ `/product/${id}` }
+          data-testid="product-detail-link"
+        >
+          <img src={ thumbnail } alt={ title } />
+          <h2>{title}</h2>
+          <h3>{price}</h3>
+        </Link>
+        <button
+          type="button"
+          data-testid="product-add-to-cart"
+          onClick={ () => this.saveToLocalStorage() }
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
     );
   }
--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import { getLocalProducts } from '../services/api';
 
 export default class ShoppingCart extends Component {
+  state = {
+    productsList: [],
+    storageList: [],
+  };
+
+  componentDidMount() {
+    this.setState({
+      productsList: getLocalProducts('produtos') || [] }, this.storageFilter);
+  }
+
+  storageFilter = () => {
+    const { productsList } = this.state;
+    const storageList = [];
+    productsList.forEach((element) => {
+      if (!storageList.some((item) => item.id === element.id)) {
+        return storageList.push(element);
+      }
+    });
+    this.setState({ storageList });
+  };
+
   render() {
+    const { productsList, storageList } = this.state;
     return (
-      <p data-testid="shopping-cart-empty-message">
-        Seu carrinho está vazio
-      </p>
+      <div>
+        { productsList.length === 0 ? (
+          <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        ) : (
+          storageList.map((element) => (
+            <div key={ element.id }>
+              <h2 data-testid="shopping-cart-product-name">{element.title}</h2>
+              <h3>{element.price}</h3>
+              <h3 data-testid="shopping-cart-product-quantity">
+                {productsList.filter((element2) => element2.id === element.id).length}
+              </h3>
+            </div>
+          ))
+        )}
+      </div>
     );
   }
 }
