e06316adee39e73c2d5ddd59bc9b47e63e9b6443
igopereira1 <igopereira1@gmail.com>
2022-09-12 19:26:20 -0300

    Resolução req 09

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { getProductById } from '../services/api';
+import { getProductById, setLocalProducts, getLocalProducts } from '../services/api';
 
 export default class Product extends Component {
   state = {
@@ -18,6 +18,25 @@ export default class Product extends Component {
     const product = await getProductById(id);
     const { attributes } = product;
     this.setState({ attributes, product });
+  };
+
+  saveToLocalStorage = ({ target }) => {
+    const idProduct = target.name;
+    const storage = getLocalProducts('produtos') || [];
+    const product = this.cartItem(idProduct);
+    setLocalProducts('produtos', [...storage, product]);
+  };
+
+  cartItem = () => {
+    const { product } = this.state;
+    const { title, thumbnail, price, id } = product;
+    const itemCart = {
+      id,
+      title,
+      thumbnail,
+      price,
+    };
+    return itemCart;
   };
 
   didPressClick = () => {
@@ -46,6 +65,15 @@ export default class Product extends Component {
               Carrinho
             </button>
           </Link>
+        </div>
+        <div>
+          <button
+            data-testid="product-detail-add-to-cart"
+            type="button"
+            onClick={ this.saveToLocalStorage }
+          >
+            Adicionar ao Carrinho
+          </button>
         </div>
         <section>
           <div>
